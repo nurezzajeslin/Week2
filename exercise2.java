fcee374c945879f2f0a51b809508c0cca6971e8f
nurezzajeslin <72841306+nurezzajeslin@users.noreply.github.com>
2020-11-15 16:44:06 +0800

    Add files via upload

---
+++ exercise2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class exercise2{
+
+	public static void main(String[] arg)
+	{
+	
+		String s1 = "Hello " + 3 + 2+1;
+		String s2 = 8+8 + " ";
+		System.out.println(s1);
+		System.out.println(s2);
+	}
+}
